a0fcf71cbe06156dc15e2515cd53d8ef3c70caed
keetz98 <47467967+keetz98@users.noreply.github.com>
2019-02-09 15:43:19 +0530

    Create vo.c

---
+++ vo.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    char a;
+    char v[]={'a','e','i','o','u','A','E','I','O','U'};
+    char c[]={'b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z','B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z'};
+    scanf("%c",&a);
+    for(i=0;i<=10;i++)
+    {
+        if(a==v[i])
+        {
+            printf("Vowel");
+        }
+        else if(a==c[i])
+        {
+            printf("Consonant");
+        }
+        else
+        {
+            printf("Invalid");
+        }
+        break;
+        
+    }
+    return 0;
+}
