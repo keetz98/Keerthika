09cc44bd81c5325826f5c30d46648f5c4c026833
keetz98 <47467967+keetz98@users.noreply.github.com>
2019-02-09 15:29:42 +0530

    Update positive.c

--- positive.c
+++ positive.c
@@ -2,13 +2,13 @@
 
 int main()
 {
-   	int n;
-	scanf("%d",&n);
-	if(n<0)
+   	int number;
+	scanf("%d",&number);
+	if(number<0)
 	{
 		printf("Negative");
 	}
-	else if(n==0)
+	else if(number==0)
 	{
 		printf("Zero");
 	}
